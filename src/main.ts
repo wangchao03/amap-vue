81ac90e641d9059ec29433dd5d8caa034987dd66
wangchao <wangchao@bizseer.com>
2021-04-28 15:32:58 +0800

    引入antd-vue

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+// 需要添加chrome插件 vue devtools bete版 并禁用原来的devtools
 
-createApp(App).use(store).use(router).mount("#app");
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import 'ant-design-vue/dist/antd.css';
+
+import { Button, message } from 'ant-design-vue';
+
+const app = createApp(App);
+app.use(store).use(router).mount('#app');
+
+/* Automatically register components under Button, such as Button.Group */
+app.use(Button);
+
+app.config.globalProperties.$message = message;
